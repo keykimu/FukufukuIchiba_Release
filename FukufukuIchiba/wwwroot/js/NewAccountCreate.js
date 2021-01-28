40829cdcd06a9c6d6b308949ffbf77fe6f7af300
keykimu <kimuyuu0309@gmail.com>
2021-01-28 11:46:16 +0900

    a
    フォルダのアップロード

---
+++ FukufukuIchiba/wwwroot/js/NewAccountCreate.js
@@ -0,0 +1,18 @@
+﻿/*******************************************************************
+ * アカウント作成クラス
+ ********************************************************************/
+
+var NewAccountCreate = {};
+
+function check() {
+    if (accountcreate_form.mail.value == "" || accountcreate_form.nickname.value == ""
+        || accountcreate_form.password.value == ""
+        || accountcreate_form.password2.value == "") {
+        //条件に一致する場合(メールアドレスとパスワードが空の場合)
+        alert("ニックネームとメールアドレスとパスワードを入力してください");    //エラーメッセージを出力
+        return false;    //送信ボタン本来の動作をキャンセルします
+    } else {
+        //条件に一致しない場合(メールアドレスが入力されている場合)
+        return true;    //送信ボタン本来の動作を実行します
+    }
+}
---
+++ FukufukuIchiba/wwwroot/js/出品ページ.js
@@ -0,0 +1,86 @@
+﻿
+    var obj1 = document.getElementById("selfile");
+obj1.addEventListener("change", function(evt){
+  var file = evt.target.files;
+  var reader = new FileReader();
+
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg").innerHTML = "<img src='" + dataUrl + "'>";
+    }
+    },false);
+ 
+        var obj2 = document.getElementById("selfile1");
+obj2.addEventListener("change", function(evt){
+  var file = evt.target.files;
+  var reader = new FileReader();
+
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg1").innerHTML = "<img src='" + dataUrl + "'>";
+        }
+        },false);
+
+            var obj3 = document.getElementById("selfile2");
+obj3.addEventListener("change", function(evt){
+  var file = evt.target.files;
+  var reader = new FileReader();
+
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg2").innerHTML = "<img src='" + dataUrl + "'>";
+            }
+}, false);
+
+                var obj4 = document.getElementById("selfile3");
+obj4.addEventListener("change", function(evt){
+  var file = evt.target.files;
+  var reader = new FileReader();
+
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg3").innerHTML = "<img src='" + dataUrl + "'>";
+                }
+                },false);
+ 
+                    var obj5 = document.getElementById("selfile4");
+obj5.addEventListener("change", function(evt){
+  var file = evt.target.files;
+  var reader = new FileReader();
+
+  //dataURL形式でファイルを読み込む
+  reader.readAsDataURL(file[0]);
+
+  //ファイルの読込が終了した時の処理
+  reader.onload = function(){
+    var dataUrl = reader.result;
+
+    //読み込んだ画像とdataURLを書き出す
+    document.getElementById("bg4").innerHTML = "<img src='" + dataUrl + "'>";
+                    }
+                    },false);
+              
